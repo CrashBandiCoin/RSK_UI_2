0075479ecd82634e10b06ef74bc3756d820703a2
yose2&- <youness.chetoui@gmail.com>
2021-09-11 12:20:21 +0200

    css modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -4,16 +4,10 @@ const Modal = (props) => {
     console.log(props.show);
     return (
         <div>
-
-
-            <div className="modal-wrapper"
-                style={{
-                transform: props.show ? 'translateY(0vh)' : 'translateY(-100vh)',
-                opacity: props.show ? true : false
-            }}>
-            <h2>test</h2>
-            <button className="btn btn-primary rounded-pill"
-            type="button" onClick={props.close}>close</button>
+            <div className="modal-fluid"
+                style={{display: props.show ? 'block' : 'none'}}>
+                <h2>test</h2>
+                <button className="btn btn-primary rounded-pill"type="button" onClick={props.close}>close</button>
             </div>
         </div>
     )
